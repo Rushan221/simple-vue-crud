d784164477e39adb71f19dfd1349d2e7abae2c63
Rushan Tamrakar <rushan2072@gmail.com>
2020-07-26 18:07:51 +0545

    crud complete

---
+++ src/components/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis ante ut felis facilisis tincidunt. Cras vel posuere ex, eu accumsan leo. Donec ac lorem tempor nisl accumsan cursus et non leo. Pellentesque aliquam eros quis ex sodales, sit amet lacinia ante fringilla. Fusce sodales urna sed ex lobortis, et cursus nulla tincidunt. Donec varius ante nec imperdiet ullamcorper. Integer imperdiet dapibus elit. Aliquam erat volutpat. In eget purus ex.</p>
+  </div>
+</template>
