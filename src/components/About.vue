469c2debaf153052e20b5cf0c2f9d8ea58903a42
Rushan Tamrakar <rushan2072@gmail.com>
2020-07-26 18:44:25 +0545

    text change

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+    <h2>Hello, the is about section</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis ante ut felis facilisis tincidunt. Cras vel posuere ex, eu accumsan leo. Donec ac lorem tempor nisl accumsan cursus et non leo. Pellentesque aliquam eros quis ex sodales, sit amet lacinia ante fringilla. Fusce sodales urna sed ex lobortis, et cursus nulla tincidunt. Donec varius ante nec imperdiet ullamcorper. Integer imperdiet dapibus elit. Aliquam erat volutpat. In eget purus ex.</p>
   </div>
 </template>
